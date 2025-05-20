6352bceb5d4f5151dfb472bf41cf69e67242a6e3
yusuf gök <gok.yusuf@protonmail.com>
2025-05-20 11:16:41 +0300

    added the objects&scripts

---
+++ Dodging the Obstacles/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]float xValue = 0.01f;
+    [SerializeField]float yValue = 0f;
+    [SerializeField]float zValue = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue,yValue,zValue);
+
+    }
+}

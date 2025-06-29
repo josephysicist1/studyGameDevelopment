d3073a8b7bb43e802a5ae146214b8a71dc61afa3
yusuf gök <gok.yusuf@protonmail.com>
2025-06-29 13:17:38 +0300

    added method with parameter & Cinemachine

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] int thrustStrength = 1000;
+    [SerializeField] float rotationStrength = 100f;
     Rigidbody rb;
     void Start()
     {
@@ -11,12 +14,37 @@ public class Movement : MonoBehaviour
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
     private void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
-            Debug.Log("THRUST!!!");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput > 0)
+        {
+            // transform.Rotate(Vector3.back * rotationStrength * Time.fixedDeltaTime); 
+            // transform.Rotate(-Vector3.forward);
+            ApplyRotation(-rotationStrength);     
+        }
+        else if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+    }
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
 }
+    

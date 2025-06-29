a341539c139a0d9c8507abdf1d04c91cdecce7d1
yusuf gök <gok.yusuf@protonmail.com>
2025-06-29 15:02:34 +0300

    added audio files and arrange the code

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -6,9 +6,11 @@ public class Movement : MonoBehaviour
     [SerializeField] InputAction rotation;
     [SerializeField] int thrustStrength = 1000;
     [SerializeField] float rotationStrength = 100f;
+    AudioSource audioSource;
     Rigidbody rb;
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
     }
     void OnEnable()
@@ -26,6 +28,14 @@ public class Movement : MonoBehaviour
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+            if (!audioSource.isPlaying)
+            {   
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
     private void ProcessRotation()
@@ -44,7 +54,9 @@ public class Movement : MonoBehaviour
     }
     private void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
     

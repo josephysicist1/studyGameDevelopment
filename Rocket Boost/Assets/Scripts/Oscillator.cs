4d0a681c6314f0e6029a50e3017e4dde917ff509
yusuf gök <gok.yusuf@protonmail.com>
2025-07-02 00:30:53 +0300

    added oscillations & light sources

---
+++ Rocket Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float speed;
+    Vector3 startPosition;
+    Vector3 endPosition;
+    float movementFactor;
+
+    void Start()
+    {
+        startPosition = transform.position; // it gives current position
+        endPosition = startPosition + movementVector;
+    }
+    void Update()
+    {
+        movementFactor = Mathf.PingPong(Time.time * speed, 1f);
+        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+    }
+}
